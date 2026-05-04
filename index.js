cc7e8a68c71ee9955289d5a426610d9873cc6894
LiamKirkland <liam.b.kirkland@gmail.com>
2026-05-04 15:12:46 -0400

    Arithmetic Practice

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let num3 = 32;
+let num4 = 7;
+const multiply = num1 * num2;
+let random = Math.floor(Math.random() * 100) + 1;
+let mod = num3 % num4;
+let max = Math.max(1, 6, 7, 9, 13, 20)
